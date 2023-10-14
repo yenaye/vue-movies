7caa95f8de2db0d99aa815f81069ba1897143a86
yenaye <rkawk4g@naver.com>
2023-10-14 22:42:13 +0900

    first commit

---
+++ src/components/MovieList.vue
@@ -0,0 +1,16 @@
+<script setup>
+import MovieCard from './MovieCard.vue';
+
+defineProps(['movies']); // 영화 정보 리스트
+</script>
+
+<template>
+    <section class="max-w-7xl mx-auto py-7">
+        <div class="flex justify-start flex-wrap">
+            <movie-card v-for="movie in movies" :key="movie.id" :movie="movie" />
+        </div>
+    </section>
+</template>
+
+<style scoped>
+</style>
